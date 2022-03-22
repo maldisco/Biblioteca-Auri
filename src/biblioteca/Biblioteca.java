3ed0a5d45f0279721683b86e745de7c345c64c54
Filipe <44625571+maldisco@users.noreply.github.com>
2022-03-22 18:07:36 -0300

    Frame inicial básica

--- src/biblioteca/Biblioteca.java
+++ src/biblioteca/Biblioteca.java
@@ -1,9 +1,28 @@
 package biblioteca;
 
-public class Biblioteca {
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 
+public class Biblioteca {
+    
     public static void main(String[] args) {
-        // A desenvolver
+        AuriFrame auri = new AuriFrame();   // criação da frame
     }
     
+}
+
+class AuriFrame extends JFrame {
+        
+    AuriFrame(){
+        this.setTitle("Auri");  // nome da aplicação
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    // fechar ao clicar no X
+        this.setResizable(false);   // tornar não redimensionável
+        this.setSize(800,800);  // tamanho da frame
+        this.setVisible(true);  // fazer a frame visível
+            
+        ImageIcon lua = new ImageIcon("resources/auri-logo.png");  // criar um imageicon
+        this.setIconImage(lua.getImage());       // muda4r icone da frame
+        this.getContentPane().setBackground(new Color(0x123456));   // cor do background  
+    }
 }
