a91a0cee11e184666ce8c88eaa80585b62ab6c48
Filipe <44625571+maldisco@users.noreply.github.com>
2022-03-24 20:02:44 -0300

    update login screen

--- src/biblioteca/Biblioteca.java
+++ src/biblioteca/Biblioteca.java
@@ -1,9 +1,12 @@
 package biblioteca;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.Insets;
 import javax.swing.BorderFactory;
@@ -18,39 +21,14 @@ import javax.swing.JTextField;
 public class Biblioteca {
     
     public static void main(String[] args) {
-        JTextField login = new JTextField();   // login textfield
-        login.setPreferredSize(new Dimension(250, 60)); // tamanho
-        login.setBackground(new Color(0x123456));   // cor do fundo
-        login.setForeground(Color.BLACK);   // cor da fonte
-        login.setFont(new Font("Arial", Font.PLAIN, 20));   // fonte
-        login.setBorder(javax.swing.BorderFactory.createCompoundBorder(
-        javax.swing.BorderFactory.createTitledBorder(
-            BorderFactory.createLineBorder(Color.BLACK, 3, true),
-            "CPF",
-            javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
-            javax.swing.border.TitledBorder.DEFAULT_POSITION,
-            new java.awt.Font("Verdana", 1, 15),
-            Color.BLACK
-        ),
-          javax.swing.BorderFactory.createEmptyBorder(3, 3, 3, 3)
-        ));  // cor e formato da borda
+       
+        JPanel loginPanel = new JPanel();
+        loginPanel.setBackground(new Color(0x123456));
+        loginPanel.setBounds(750, 250, 300, 500);
+        loginPanel.setLayout(new GridBagLayout());
         
-        JTextField senha = new JTextField();   // senha textfield
-        senha.setPreferredSize(new Dimension(250, 60)); // tamanho
-        senha.setBackground(new Color(0x123456));   // cor do fundo
-        senha.setForeground(Color.BLACK);   // cor da fonte
-        senha.setFont(new Font("Arial", Font.PLAIN, 20));   // fonte
-        senha.setBorder(javax.swing.BorderFactory.createCompoundBorder(
-        javax.swing.BorderFactory.createTitledBorder(
-            BorderFactory.createLineBorder(Color.BLACK, 3, true),
-            "Senha",
-            javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
-            javax.swing.border.TitledBorder.DEFAULT_POSITION,
-            new java.awt.Font("Verdana", 1, 15),
-            Color.BLACK
-        ),
-          javax.swing.BorderFactory.createEmptyBorder(3, 3, 3, 3)
-        ));  // cor e formato da borda
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
         
         ImageIcon moon = new ImageIcon("resources/moon.png"); 
         Image newMoon = moon.getImage(); 
@@ -67,21 +45,66 @@ public class Biblioteca {
         label.setFont(new Font("Arial", Font.BOLD, 40));
         label.setVerticalAlignment(JLabel.CENTER); // posição vertical
         label.setHorizontalAlignment(JLabel.CENTER); // posição horizontal
+        c.gridx = 0;
+        c.gridy = 0;
+        loginPanel.add(label, c);
         
+        JTextField login = new JTextField();   // login textfield
+        login.setPreferredSize(new Dimension(500, 50));
+        login.setBackground(new Color(0x123456));   // cor do fundo
+        login.setForeground(Color.BLACK);   // cor da fonte
+        login.setFont(new Font("Arial", Font.PLAIN, 20));   // fonte
+        login.setBorder(javax.swing.BorderFactory.createCompoundBorder(
+        javax.swing.BorderFactory.createTitledBorder(
+            BorderFactory.createLineBorder(Color.BLACK, 3, true),
+            "CPF",
+            javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
+            javax.swing.border.TitledBorder.DEFAULT_POSITION,
+            new java.awt.Font("Verdana", 1, 15),
+            Color.BLACK
+        ),
+          javax.swing.BorderFactory.createEmptyBorder(3, 3, 3, 3)
+        ));  // cor e formato da borda
+        c.gridx = 0;
+        c.gridy = 1;
+        loginPanel.add(login, c);
+        
+        JTextField senha = new JTextField();   // senha textfield
+        senha.setPreferredSize(new Dimension(500, 50));
+        senha.setBackground(new Color(0x123456));   // cor do fundo
+        senha.setForeground(Color.BLACK);   // cor da fonte
+        senha.setFont(new Font("Arial", Font.PLAIN, 20));   // fonte
+        senha.setBorder(javax.swing.BorderFactory.createCompoundBorder(
+        javax.swing.BorderFactory.createTitledBorder(
+            BorderFactory.createLineBorder(Color.BLACK, 3, true),
+            "Senha",
+            javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
+            javax.swing.border.TitledBorder.DEFAULT_POSITION,
+            new java.awt.Font("Verdana", 1, 15),
+            Color.BLACK
+        ),
+          javax.swing.BorderFactory.createEmptyBorder(3, 3, 3, 3)
+        ));  // cor e formato da borda
+        c.gridx = 0;
+        c.gridy = 2;
+        loginPanel.add(senha, c);
+        
+           
         JButton loginButton =  new JButton();
+        loginButton.setFocusable(false);
+        loginButton.setPreferredSize(new Dimension(200, 50));
         loginButton.setText("Log in");
         loginButton.setBackground(Color.BLACK);   // cor do fundo
         loginButton.setForeground(new Color(0x123456));   // cor da fonte
+        loginButton.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         loginButton.setFont(new Font("Verdana", Font.BOLD, 30));
-        
-        JPanel loginPanel = new JPanel();
-        loginPanel.setBackground(new Color(0x123456));
-        loginPanel.setBounds(750, 250, 300, 500);
-        loginPanel.setLayout(new BoxLayout(loginPanel, BoxLayout.Y_AXIS));
-        loginPanel.add(label);
-        loginPanel.add(login);
-        loginPanel.add(senha);
-        loginPanel.add(loginButton);
+        loginButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        c.ipady = 25;
+        c.weighty = 2;
+        c.gridx = 0;
+        c.gridy = 3;
+        loginPanel.add(loginButton, c);
+
         
         JFrame auri = new JFrame();   // criação da frame
         auri.setTitle("Auri");  // nome da aplicação
